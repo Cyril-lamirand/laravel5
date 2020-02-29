3b5d32c91ba64b20a448e53d822f11fb4723e0ca
Cyril Lamirand @DEV <Contact.cyril1@gmail.com>
2020-02-29 23:36:30 +0100

    Update template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -25,9 +25,14 @@
 		                <div class="collapse navbar-collapse" id="navbarSupportedContent">
 		                    <!-- Left Side Of Navbar -->
 		                    <ul class="navbar-nav mr-auto">
+								<li class="nav-item">
+	                                <a class="nav-link" href="{{ url('clients/') }}">Clients</a>
+	                            </li>
 
+	                            <li class="nav-item">
+	                                <a class="nav-link" href="{{ url('article/1') }}">Articles</a>
+	                            </li>
 		                    </ul>
-
 		                    <!-- Right Side Of Navbar -->
 		                    <ul class="navbar-nav ml-auto">
 		                        <!-- Authentication Links -->
@@ -41,15 +46,6 @@
 		                                </li>
 		                            @endif
 		                        @else
-
-		                       		<li class="nav-item">
-		                                <a class="nav-link" href="{{ url('clients/') }}">Clients</a>
-		                            </li>
-
-		                            <li class="nav-item">
-		                                <a class="nav-link" href="{{ url('article/1') }}">Articles</a>
-		                            </li>
-
 		                            <li class="nav-item dropdown">
 		                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 		                                    {{ Auth::user()->name }} <span class="caret"></span>
